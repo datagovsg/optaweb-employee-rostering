bdc6c843851fe05809f38ae2123476d9036a3c54
LoneRifle <LoneRifle@users.noreply.github.com>
2020-02-05 10:14:01 +0800

    Create staging profile, ensure production profile is spelt out

--- optaweb-employee-rostering-backend/src/main/java/org/optaweb/employeerostering/configuration/Profiles.java
+++ optaweb-employee-rostering-backend/src/main/java/org/optaweb/employeerostering/configuration/Profiles.java
@@ -2,6 +2,7 @@ package org.optaweb.employeerostering.configuration;
 
 public class Profiles {
     public static final String DEVELOPMENT = "dev";
-    public static final String PRODUCTION = "prod";
+    public static final String PRODUCTION = "production";
+    public static final String STAGING = "staging";
     public static final String TEST = "test";
 }
